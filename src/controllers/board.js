1d2a1fb09e53ae662cc5a776330fe5c959169a28
Anna Bekkarevich <annabekkarevich@gmail.com>
2019-09-12 18:41:45 -0400

    feat: Add Sort class and edit controller

--- src/controllers/board.js
+++ src/controllers/board.js
@@ -1,13 +1,13 @@
-import {Position, createElement, render, unrender} from '../components/utils';
+import {Position, Key, createElement, render, unrender} from '../components/utils';
 import {TASKS_COUNT, TASKS_PER_PAGE} from '../components/data';
 import Board from '../components/board';
 import TaskList from '../components/task-list';
 import Task from '../components/task';
 import TaskEdit from '../components/task-edit';
 import Load from '../components/load';
+import Sort from '../components/sort';
 import {getCountByFlag} from '../components/search-filter';
 import {getMessageTemplate} from '../components/message';
-import {getFilterListTemplate, sortType} from '../components/filter-sort';
 
 class BoardController {
   constructor(container, tasks) {
@@ -16,15 +16,23 @@ class BoardController {
     this._board = new Board();
     this._taskList = new TaskList();
     this._load = new Load();
+    this._sort = new Sort();
+    this._taskPageStart = 0;
+    this._taskPageEnd = TASKS_PER_PAGE;
   }
 
+  /**
+   * Renders a task and attaches event handlers
+   * @param {Object} task
+   * @private
+   */
   _renderTask(task) {
     const taskComponent = new Task(task);
     const taskEditComponent = new TaskEdit(task);
 
     // Closes edit mode on Esc keydown
     const onEscKeyDown = (e) => {
-      if (e.key === `Escape` || e.key === `Esc`) {
+      if (e.key === Key.ESCAPE_IE || e.key === Key.ESCAPE) {
         this._taskList.getElement().replaceChild(taskComponent.getElement(), taskEditComponent.getElement());
         document.removeEventListener(`keydown`, onEscKeyDown);
       }
@@ -60,31 +68,82 @@ class BoardController {
     render(this._taskList.getElement(), taskComponent.getElement(), Position.BEFOREEND);
   }
 
+  /**
+   * Sort event handler
+   * @param {Object} e
+   * @private
+   */
+  _onSortLinkClick(e) {
+    e.preventDefault();
+    if (e.target.tagName !== `A`) {
+      return;
+    }
+
+    // Clear tasks and load button
+    this._taskList.getElement().innerHTML = ``;
+    unrender(this._load.getElement());
+
+    // Get tasks by sort type
+    switch (e.target.dataset.sortType) {
+      case `date-up`:
+        const sortedByDateUpTasks = this._tasks.slice().sort((a, b) => a.dueDate - b.dueDate);
+        this._renderTasksAndLoadButton(sortedByDateUpTasks);
+        break;
+      case `date-down`:
+        const sortedByDateDownTasks = this._tasks.slice().sort((a, b) => b.dueDate - a.dueDate);
+        this._renderTasksAndLoadButton(sortedByDateDownTasks);
+        break;
+      case `default`:
+        this._renderTasksAndLoadButton(this._tasks);
+        break;
+    }
+  }
+
+  /**
+   * Renders tasks in particular order and load more button
+   * @param {Array} tasksSequence
+   * @private
+   */
+  _renderTasksAndLoadButton(tasksSequence) {
+    let taskPageStart = this._taskPageStart;
+    let taskPageEnd = this._taskPageEnd;
+    tasksSequence.slice(taskPageStart, taskPageEnd).forEach((task) => this._renderTask(task));
+
+    // Render Load button
+    render(this._board.getElement(), this._load.getElement(), Position.BEFOREEND);
+    this._load.getElement().addEventListener(`click`, () => {
+      taskPageStart += TASKS_PER_PAGE;
+      taskPageEnd += TASKS_PER_PAGE;
+      if (taskPageEnd >= tasksSequence.length) {
+        unrender(this._load.getElement());
+      }
+      tasksSequence.slice(taskPageStart, taskPageEnd).forEach((task) => this._renderTask(task));
+    });
+  }
+
+  /**
+   * Renders page with tasks of no tasks message
+   */
   init() {
     // Show message if there are no tasks or all task are archived
     if (TASKS_COUNT === 0 || getCountByFlag(this._tasks, `isArchive`) === TASKS_COUNT) {
       render(this._container, this._board.getElement(), Position.BEFOREEND);
       render(this._board.getElement(), createElement(getMessageTemplate()), Position.BEFOREEND);
     } else {
-      // Render sort and tasks otherwise
+      // Render board container
       render(this._container, this._board.getElement(), Position.BEFOREEND);
-      render(this._board.getElement(), createElement(getFilterListTemplate(sortType)), Position.BEFOREEND);
-      render(this._board.getElement(), this._taskList.getElement(), Position.BEFOREEND);
-      let taskPageStart = 0;
-      let taskPageEnd = TASKS_PER_PAGE;
-      this._tasks.slice(taskPageStart, taskPageEnd).forEach((task) => this._renderTask(task));
 
-      // Render Load button
-      render(this._board.getElement(), this._load.getElement(), Position.BEFOREEND);
-
-      this._load.getElement().addEventListener(`click`, () => {
-        taskPageStart += TASKS_PER_PAGE;
-        taskPageEnd += TASKS_PER_PAGE;
-        if (taskPageEnd >= this._tasks.length) {
-          unrender(this._load.getElement());
-        }
-        this._tasks.slice(taskPageStart, taskPageEnd).forEach((task) => this._renderTask(task));
+      // Render sort
+      render(this._board.getElement(), this._sort.getElement(), Position.BEFOREEND);
+      this._sort.getElement().addEventListener(`click`, (e) => {
+        this._onSortLinkClick(e);
       });
+
+      // Render tasks container
+      render(this._board.getElement(), this._taskList.getElement(), Position.BEFOREEND);
+
+      // Render tasks and load button
+      this._renderTasksAndLoadButton(this._tasks);
     }
   }
 }
